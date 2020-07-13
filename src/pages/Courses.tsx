ab8d550d0d31551c11d772c315d6e55e2f78c00f
Joel Azemar <joel.azemar@gmail.com>
2020-07-13 12:16:11 +0200

    Add Dynamic Dates

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
 
-import { IonHeader, IonContent, IonToolbar, IonTitle, IonGrid, IonPage,
-  IonRow, IonCol, IonCard, IonCardContent, IonButton } from '@ionic/react';
+import {
+  IonHeader, IonContent, IonToolbar, IonTitle, IonGrid, IonPage,
+  IonRow, IonCol, IonCard, IonCardContent, IonButton,
+  IonCardHeader, IonCardTitle, IonCardSubtitle
+} from '@ionic/react';
 
 export const COURSE_DATA = [
-  { id: 'b1', title: 'English B1' },
-  { id: 'b2', title: 'English B2' },
-  { id: 'c1', title: 'English C1' },
+  {
+    id: 'b1',
+    title: 'English B1',
+    enrolled: new Date('03/22/2020'),
+    goals: [
+      { id: 'c1g1', text: 'Finish the course!' },
+      { id: 'c1g2', text: 'Learn a lot' },
+    ]
+  },
+  { id: 'b2', title: 'English B2', enrolled: new Date('01/18/2017'),
+  goals: [
+    { id: 'c2g1', text: 'Finish the course!' },
+    { id: 'c2g2', text: 'Learn a lot' },
+  ] },
+  { id: 'c1', title: 'English C1', enrolled: new Date('06/05/2015'),
+  goals: [
+    { id: 'c3g1', text: 'Finish the course!' },
+    { id: 'c3g2', text: 'Learn a lot' },
+  ] },
 ];
 
 const Courses: React.FC = () => {
@@ -25,13 +44,20 @@ const Courses: React.FC = () => {
             <IonRow key={course.id}>
               <IonCol size-md='4' offset-md='4'>
                 <IonCard>
-                  <IonCardContent className='ion-text-center'>
-                    <h2>
+                  <IonCardHeader>
+                    <IonCardTitle>
                       {course.title}
-                    </h2>
-                    <IonButton routerLink={`/courses/${course.id}`}>
-                      View Course Goals
-                    </IonButton>
+                    </IonCardTitle>
+                    <IonCardSubtitle>
+                      Enrolled on {course.enrolled.toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' })}
+                    </IonCardSubtitle>
+                  </IonCardHeader>
+                  <IonCardContent>
+                    <div className='ion-text-right'>
+                      <IonButton fill='clear' color='secondary' routerLink={`/courses/${course.id}`}>
+                        View Course Goals
+                      </IonButton>
+                    </div>
                   </IonCardContent>
                 </IonCard>
               </IonCol>
