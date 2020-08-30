9435e002d26cf1535d730c0a4f3e505f9d1a6775
Joel Azemar <joel.azemar@gmail.com>
2020-08-30 18:47:27 +0200

    8. Ionic Component Deep Dive > 15-all-goals-list

--- src/pages/AllGoals.tsx
+++ src/pages/AllGoals.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
+import React from "react";
 
-import { IonHeader, IonContent, IonToolbar, IonTitle, IonPage,
-  IonButton, IonMenuButton } from '@ionic/react';
+import {
+  IonHeader,
+  IonContent,
+  IonToolbar,
+  IonTitle,
+  IonPage,
+  IonButton,
+  IonMenuButton,
+  IonList,
+  IonItem,
+  IonLabel,
+} from "@ionic/react";
+
+import { COURSE_DATA } from "./Courses";
 
 const AllGoals: React.FC = () => {
+  const goals = COURSE_DATA.flatMap((course) => {
+    return course.goals.map(goal => {
+      return { ...goal, courseTitle: course.title };
+    });
+  });
+
+  console.log(goals);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonButton slot='start'>
+          <IonButton slot="start">
             <IonMenuButton />
           </IonButton>
-          <IonTitle>
-            Course Goals
-          </IonTitle>
+          <IonTitle>Course Goals</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <h2>
-          This works - all goals page!
-        </h2>
+        <IonList>
+          {goals.map((goal) => (
+            <IonItem key={goal.id}>
+              <IonLabel>
+                <h2>{goal.text}</h2>
+                <p>{goal.courseTitle}</p>
+              </IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
