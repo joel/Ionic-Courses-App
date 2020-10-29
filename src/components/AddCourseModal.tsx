4ad0216a907df812c3069f231d02ee4657350f1b
Joel Azemar <joel.azemar@gmail.com>
2020-10-29 15:28:52 +0100

    9. Handling User Input & Application State > 02-fetching-and-validating-user-input

--- src/components/AddCourseModal.tsx
+++ src/components/AddCourseModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import {
   IonModal,
@@ -14,11 +14,33 @@ import {
   IonInput,
   IonButton,
   IonDatetime,
+  IonText,
 } from "@ionic/react";
 
 const AddCourseModal: React.FC<{ show: boolean; onCancel: () => void }> = (
   props
 ) => {
+  const [error, setError] = useState("");
+
+  const titleRef = useRef<HTMLIonInputElement>(null);
+  const dateRef = useRef<HTMLIonDatetimeElement>(null);
+
+  const saveHandler = () => {
+    const enteredTitle = titleRef.current!.value;
+    const selectedDate = dateRef.current!.value;
+    if (
+      !enteredTitle ||
+      !selectedDate ||
+      enteredTitle.toString().trim().length === 0 ||
+      selectedDate.trim().length === 0
+    ) {
+      setError("Please enter a valid title and select a valid date.");
+      return;
+    }
+
+    setError("");
+  };
+
   return (
     <IonModal isOpen={props.show}>
       <IonHeader>
@@ -32,7 +54,7 @@ const AddCourseModal: React.FC<{ show: boolean; onCancel: () => void }> = (
             <IonCol>
               <IonItem>
                 <IonLabel position="floating">Course Title</IonLabel>
-                <IonInput type="text" />
+                <IonInput type="text" ref={titleRef} />
               </IonItem>
             </IonCol>
           </IonRow>
@@ -40,10 +62,19 @@ const AddCourseModal: React.FC<{ show: boolean; onCancel: () => void }> = (
             <IonCol>
               <IonItem>
                 <IonLabel>Enrollement Date</IonLabel>
-                <IonDatetime displayFormat="DD MM YYYY" />
+                <IonDatetime displayFormat="DD MM YYYY" ref={dateRef} />
               </IonItem>
             </IonCol>
           </IonRow>
+          {error && (
+            <IonRow className="ion-text-center">
+              <IonCol>
+                <IonText color="danger">
+                  <p>{error}</p>
+                </IonText>
+              </IonCol>
+            </IonRow>
+          )}
           <IonRow className="ion-text-center">
             <IonCol>
               <IonButton color="dark" fill="clear" onClick={props.onCancel}>
@@ -51,7 +82,7 @@ const AddCourseModal: React.FC<{ show: boolean; onCancel: () => void }> = (
               </IonButton>
             </IonCol>
             <IonCol>
-              <IonButton expand="block" color="secondary">
+              <IonButton expand="block" color="secondary" onClick={saveHandler}>
                 Save
               </IonButton>
             </IonCol>
