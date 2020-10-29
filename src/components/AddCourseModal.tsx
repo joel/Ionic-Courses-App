c1df55330e5fd29a4815fb11146e990922c9af75
Joel Azemar <joel.azemar@gmail.com>
2020-10-29 15:10:54 +0100

    Update AddCourseModal.tsx

--- src/components/AddCourseModal.tsx
+++ src/components/AddCourseModal.tsx
@@ -1,53 +1,65 @@
 import React from "react";
 
-import { IonModal, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonItem, IonLabel, IonInput, IonButton, IonDatetime } from "@ionic/react";
+import {
+  IonModal,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonButton,
+  IonDatetime,
+} from "@ionic/react";
 
-const AddCourseModal: React.FC<{show: boolean; onCancel: () => void;}> = props => {
-  return <IonModal isOpen={props.show}>
+const AddCourseModal: React.FC<{ show: boolean; onCancel: () => void }> = (
+  props
+) => {
+  return (
+    <IonModal isOpen={props.show}>
       <IonHeader>
-          <IonToolbar>
-              <IonTitle>
-                  Add Course
-              </IonTitle>
-          </IonToolbar>
+        <IonToolbar>
+          <IonTitle>Add Course</IonTitle>
+        </IonToolbar>
       </IonHeader>
       <IonContent>
-          <IonGrid>
-              <IonRow>
-                  <IonCol>
-                    <IonItem>
-                        <IonLabel position='floating'>
-                            Course Title
-                        </IonLabel>
-                        <IonInput type='text' />
-                    </IonItem>
-                  </IonCol>
-              </IonRow>
-              <IonRow>
-                  <IonCol>
-                      <IonItem>
-                          <IonLabel>
-                              Enrollement Date
-                          </IonLabel>
-                          <IonDatetime displayFormat='DD MM YYYY' />
-                      </IonItem>
-                  </IonCol>
-              </IonRow>
-              <IonRow className='ion-text-center'>
-                  <IonCol>
-                      <IonButton color='dark' fill='clear' onClick={props.onCancel}>
-                          Cancel
-                      </IonButton>
-                  </IonCol>
-                  <IonCol>
-                      <IonButton expand='block' color='secondary'>
-                          Save
-                      </IonButton>
-                  </IonCol>
-              </IonRow>
-          </IonGrid>
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="floating">Course Title</IonLabel>
+                <IonInput type="text" />
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel>Enrollement Date</IonLabel>
+                <IonDatetime displayFormat="DD MM YYYY" />
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow className="ion-text-center">
+            <IonCol>
+              <IonButton color="dark" fill="clear" onClick={props.onCancel}>
+                Cancel
+              </IonButton>
+            </IonCol>
+            <IonCol>
+              <IonButton expand="block" color="secondary">
+                Save
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
-  </IonModal>;
-}; 
+    </IonModal>
+  );
+};
 
 export default AddCourseModal;
